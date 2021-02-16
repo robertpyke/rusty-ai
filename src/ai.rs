20acc28443bab138abac8630ea2ae2643fdb0d90
Robert Pyke <robert.j.pyke@gmail.com>
2021-02-15 20:37:23 -0800

    Update based on: https://sunjay.dev/learn-game-dev/creating-a-project.html . Added AI to track Enemy. Updated dependencies.

---
+++ src/ai.rs
@@ -0,0 +1,120 @@
+use specs::prelude::*;
+use rand::prelude::*;
+
+use crate::components::*;
+
+const ENEMY_MOVEMENT_SPEED: i32 = 7;
+const HERO_MOVEMENT_SPEED: i32 = 4;
+
+pub struct AI;
+
+impl<'a> System<'a> for AI {
+    type SystemData = (
+        ReadStorage<'a, AIControlled>,
+        ReadStorage<'a, Enemy>,
+        ReadStorage<'a, Hero>,
+        ReadStorage<'a, Position>,
+        WriteStorage<'a, Velocity>,
+    );
+
+    fn run(&mut self, mut data: Self::SystemData) {
+        //TODO: This code can be made nicer and more idiomatic using more pattern matching.
+        // Look up "rust irrefutable patterns" and use them here.
+        let mut rng = thread_rng();
+        for (_, _, vel) in (&data.0, &data.1, &mut data.4).join() {
+            if rng.gen_range(0..2) == 0 {
+                vel.speed = ENEMY_MOVEMENT_SPEED;
+                vel.direction = match rng.gen_range(0..4) {
+                    0 => Direction::Up,
+                    1 => Direction::Down,
+                    2 => Direction::Left,
+                    3 => Direction::Right,
+                    _ => unreachable!(),
+                }
+            }
+        }
+
+        for (_, _, hero_pos, hero_vel) in (&data.0, &data.2, &data.3, &mut data.4).join() {
+            let hero_x = hero_pos.0.x;
+            let hero_y = hero_pos.0.y;
+
+            // Find the nearest enemy
+            let mut nearest_enemy_pos: Option<Position> = None;
+            let mut nearest_distance = f32::MAX;
+            for (_, enemy_pos) in (&data.1, &data.3).join() {
+                let enemy_x = enemy_pos.0.x;
+                let enemy_y = enemy_pos.0.y;
+
+                let x_delta = (enemy_x - hero_x).abs();
+                let y_delta = (enemy_y - hero_y).abs();
+                let c_squared = (x_delta.pow(2) + y_delta.pow(2)) as f32;
+                let distance = c_squared.sqrt();
+
+                match nearest_enemy_pos {
+                    None | _ if distance < nearest_distance => {
+                        nearest_enemy_pos = Some(enemy_pos.clone());
+                        nearest_distance = distance;
+                    },
+                    _ => {
+                        // no-op (not nearer)
+                    }
+                }
+            }
+
+            match nearest_enemy_pos {
+                Some(nearest_enemy_pos) => {
+                    println!("Nearest enemy: {:?}", nearest_enemy_pos);
+                    let enemy_x = nearest_enemy_pos.0.x;
+                    let enemy_y = nearest_enemy_pos.0.y;
+                    let x_delta = enemy_x - hero_x;
+                    let x_delta_abs = x_delta.abs();
+                    let y_delta = enemy_y - hero_y;
+                    let y_delta_abs = y_delta.abs();
+                    match x_delta_abs >= y_delta_abs {
+                        true if x_delta_abs > 0 => {
+                            // head in the X
+                            hero_vel.speed = match x_delta_abs {
+                                0..=HERO_MOVEMENT_SPEED => x_delta_abs,
+                                _ => HERO_MOVEMENT_SPEED
+                            };
+                            match x_delta {
+                                i32::MIN..=-1 => {
+                                    hero_vel.direction = Direction::Left;
+                                }
+                                0..=i32::MAX => {
+                                    hero_vel.direction = Direction::Right;
+                                },
+                            }
+                        }, 
+                        false if y_delta_abs > 0 => {
+                            // head in the Y
+                            hero_vel.speed = match y_delta_abs {
+                                0..=HERO_MOVEMENT_SPEED => y_delta_abs,
+                                _ => HERO_MOVEMENT_SPEED
+                            };
+                            match y_delta {
+                                i32::MIN..=-1 => {
+                                    hero_vel.direction = Direction::Up;
+                                }
+                                0..=i32::MAX => {
+                                    hero_vel.direction = Direction::Down;
+                                },
+                            }
+                        },
+                        _ => {
+                            println!("Not moving, at enemy.");
+                            // Stop Moving
+                            hero_vel.speed = 0;
+                        }
+                    }
+                }
+                
+                None => {
+                    println!("Not moving, no enemy.");
+                    // Stop Moving
+                    hero_vel.speed = 0;
+                }
+            }
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,163 @@
-use sdl2::pixels::Color;
+mod components;
+mod physics;
+mod animator;
+mod keyboard;
+mod renderer;
+mod ai;
+use rand::prelude::*;
+
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
-use sdl2::render::WindowCanvas;
+use sdl2::pixels::Color;
+use sdl2::rect::{Point, Rect};
+// "self" imports the "image" module itself as well as everything else we listed
+use sdl2::image::{self, LoadTexture, InitFlag};
+
+use specs::prelude::*;
+
 use std::time::Duration;
 
-fn render(canvas: &mut WindowCanvas, color: Color) {
-    canvas.set_draw_color(color);
-    canvas.clear();
-    canvas.present();
+use crate::components::*;
+
+pub enum MovementCommand {
+    Stop,
+    Move(Direction),
+}
+
+/// Returns the row of the spritesheet corresponding to the given direction
+fn direction_spritesheet_row(direction: Direction) -> i32 {
+    use self::Direction::*;
+    match direction {
+        Up => 3,
+        Down => 0,
+        Left => 1,
+        Right => 2,
+    }
+}
+
+/// Create animation frames for the standard character spritesheet
+fn character_animation_frames(spritesheet: usize, top_left_frame: Rect, direction: Direction) -> Vec<Sprite> {
+    // All assumptions about the spritesheets are now encapsulated in this function instead of in
+    // the design of our entire system. We can always replace this function, but replacing the
+    // entire system is harder.
+
+    let (frame_width, frame_height) = top_left_frame.size();
+    let y_offset = top_left_frame.y() + frame_height as i32 * direction_spritesheet_row(direction);
+
+    let mut frames = Vec::new();
+    for i in 0..3 {
+        frames.push(Sprite {
+            spritesheet,
+            region: Rect::new(
+                top_left_frame.x() + frame_width as i32 * i,
+                y_offset,
+                frame_width,
+                frame_height,
+            ),
+        })
+    }
+
+    frames
+}
+
+fn initialize_player(world: &mut World, player_spritesheet: usize) {
+    let player_top_left_frame = Rect::new(0, 0, 26, 36);
+
+    let player_animation = MovementAnimation {
+        current_frame: 0,
+        up_frames: character_animation_frames(player_spritesheet, player_top_left_frame, Direction::Up),
+        down_frames: character_animation_frames(player_spritesheet, player_top_left_frame, Direction::Down),
+        left_frames: character_animation_frames(player_spritesheet, player_top_left_frame, Direction::Left),
+        right_frames: character_animation_frames(player_spritesheet, player_top_left_frame, Direction::Right),
+    };
+
+    world.create_entity()
+        .with(AIControlled)
+        .with(Hero)
+        .with(Position(Point::new(thread_rng().gen_range(-200..200), thread_rng().gen_range(-200..200))))
+        .with(Velocity {speed: 0, direction: Direction::Right})
+        .with(player_animation.right_frames[0].clone())
+        .with(player_animation)
+        .build();
+}
+
+fn initialize_enemy(world: &mut World, enemy_spritesheet: usize, position: Point) {
+    let enemy_top_left_frame = Rect::new(0, 0, 32, 36);
+
+    let enemy_animation = MovementAnimation {
+        current_frame: 0,
+        up_frames: character_animation_frames(enemy_spritesheet, enemy_top_left_frame, Direction::Up),
+        down_frames: character_animation_frames(enemy_spritesheet, enemy_top_left_frame, Direction::Down),
+        left_frames: character_animation_frames(enemy_spritesheet, enemy_top_left_frame, Direction::Left),
+        right_frames: character_animation_frames(enemy_spritesheet, enemy_top_left_frame, Direction::Right),
+    };
+
+    world.create_entity()
+        .with(AIControlled)
+        .with(Enemy)
+        .with(Position(position))
+        .with(Velocity {speed: 0, direction: Direction::Right})
+        .with(enemy_animation.right_frames[0].clone())
+        .with(enemy_animation)
+        .build();
 }
 
 fn main() -> Result<(), String> {
     let sdl_context = sdl2::init()?;
     let video_subsystem = sdl_context.video()?;
+    // Leading "_" tells Rust that this is an unused variable that we don't care about. It has to
+    // stay unused because if we don't have any variable at all then Rust will treat it as a
+    // temporary value and drop it right away!
+    let _image_context = image::init(InitFlag::PNG | InitFlag::JPG)?;
 
-    let window = video_subsystem.window("rusty-ai", 800, 600)
+    let window = video_subsystem.window("game tutorial", 800, 600)
         .position_centered()
         .build()
         .expect("could not initialize video subsystem");
 
     let mut canvas = window.into_canvas().build()
         .expect("could not make a canvas");
+    let texture_creator = canvas.texture_creator();
+
+    let mut dispatcher = DispatcherBuilder::new()
+        .with(keyboard::Keyboard, "Keyboard", &[])
+        .with(ai::AI, "AI", &[])
+        .with(physics::Physics, "Physics", &["Keyboard", "AI"])
+        .with(animator::Animator, "Animator", &["Keyboard", "AI"])
+        .build();
+
+    let mut world = World::new();
+    dispatcher.setup(&mut world);
+    renderer::SystemData::setup(&mut world);
+
+    // Initialize resource
+    let movement_command: Option<MovementCommand> = None;
+    world.insert(movement_command);
+
+    let textures = [
+        texture_creator.load_texture("assets/bardo.png")?,
+        texture_creator.load_texture("assets/reaper.png")?,
+    ];
+    // First texture in textures array
+    let player_spritesheet = 0;
+    // Second texture in the textures array
+    let enemy_spritesheet = 1;
+
+    initialize_player(&mut world, player_spritesheet);
+
+    initialize_enemy(&mut world, enemy_spritesheet, Point::new(-150, -150));
+    initialize_enemy(&mut world, enemy_spritesheet, Point::new(150, -190));
+    initialize_enemy(&mut world, enemy_spritesheet, Point::new(-150, 170));
+    initialize_enemy(&mut world, enemy_spritesheet, Point::new(-150, 170));
+    initialize_enemy(&mut world, enemy_spritesheet, Point::new(-150, 170));
+    initialize_enemy(&mut world, enemy_spritesheet, Point::new(-150, 170));
+    initialize_enemy(&mut world, enemy_spritesheet, Point::new(-150, 170));
 
     let mut event_pump = sdl_context.event_pump()?;
+    let mut i = 0;
     'running: loop {
+        // None - no change, Some(MovementCommand) - perform movement
+        let mut movement_command = None;
         // Handle events
         for event in event_pump.poll_iter() {
             match event {
@@ -31,15 +165,40 @@ fn main() -> Result<(), String> {
                 Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
                     break 'running;
                 },
+                Event::KeyDown { keycode: Some(Keycode::Left), repeat: false, .. } => {
+                    movement_command = Some(MovementCommand::Move(Direction::Left));
+                },
+                Event::KeyDown { keycode: Some(Keycode::Right), repeat: false, .. } => {
+                    movement_command = Some(MovementCommand::Move(Direction::Right));
+                },
+                Event::KeyDown { keycode: Some(Keycode::Up), repeat: false, .. } => {
+                    movement_command = Some(MovementCommand::Move(Direction::Up));
+                },
+                Event::KeyDown { keycode: Some(Keycode::Down), repeat: false, .. } => {
+                    movement_command = Some(MovementCommand::Move(Direction::Down));
+                },
+                Event::KeyUp { keycode: Some(Keycode::Left), repeat: false, .. } |
+                Event::KeyUp { keycode: Some(Keycode::Right), repeat: false, .. } |
+                Event::KeyUp { keycode: Some(Keycode::Up), repeat: false, .. } |
+                Event::KeyUp { keycode: Some(Keycode::Down), repeat: false, .. } => {
+                    movement_command = Some(MovementCommand::Stop);
+                },
                 _ => {}
             }
         }
 
+        *world.write_resource() = movement_command;
+
+        // Update
+        i = (i + 1) % 255;
+        dispatcher.dispatch(&mut world);
+        world.maintain();
+
         // Render
-        render(&mut canvas, Color::RGB(255, 255, 255));
+        renderer::render(&mut canvas, Color::RGB(i, 64, 255 - i), &textures, world.system_data())?;
 
         // Time management!
-        ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60));
+        ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 20));
     }
 
     Ok(())
